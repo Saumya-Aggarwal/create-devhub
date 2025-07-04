a10624669e50bf76e616f3c5803bbd8e79e47398
Saumya-Aggarwal <saumyaaggarwal667@gmail.com>
2025-07-04 15:14:28 +0530

    Clean up repository - remove node_modules

--- dist/bin/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { askUserOptions } from "../core/prompts.js";
-import { runGenerator } from "../core/generator.js";
-console.log('🐣 create‑devhub starting…');
-async function run() {
-    const opts = await askUserOptions();
-    await runGenerator(opts);
-}
-run();
